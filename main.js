885b57a029891bf0407cc05abc31770c72ffeca2
xxmagnoxx123 <magnoxhot@hotmail.com>
2025-01-27 20:16:11 -0300

    ajuste no codigo

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const form = document.getElementById('formulario');
 
 function validaNome(nomeCompleto){
     const nomeComArray = nomeCompleto.split(' ');
-    return nomeComArray.legth >= 2;
+    return nomeComArray.length >= 2;
 }
 
 form.addEventListener('submit', function(e) {
     let formValido = false;
     e.preventDefault();
 
-    const nomeCompleto = document.getElementById('nome-completo');
+    const nomeCompleto = document.getElementById('nome-completo').value;
+
+    formValido = validaNome(nomeCompleto)
+
     if (formValido) {
         alert('Está tudo certo!')
     }else{
